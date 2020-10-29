76006717b265e081315e17b2b8ef3ca7a5c6a122
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:25:57 -0230

    Add files via upload

---
+++ DirectoryInformation/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using System.IO;
+
+
+namespace DirectoryInformation
+{
+    class Directory
+    {
+        static void Main()
+        {
+            WriteLine("Press enter once to begin testing your directory's");
+            string directoryName = ReadLine();
+
+
+            while (directoryName != "end")
+            {
+                Write("\nEnter a Directory name: ");
+                string directory = ReadLine();
+                string fulldir = Path.Combine(directoryName.TrimEnd('/') + "/", directory);
+
+                if (directory.ToLower() == "end")
+                    break;
+
+                else if (System.IO.Directory.Exists(fulldir))
+                {
+                    foreach (string file in System.IO.Directory.GetFiles(fulldir))
+                        WriteLine(file);
+                }
+
+                else
+                    WriteLine("Directory {0} does not exist", directory);
+            }
+        }
+
+    }
+}
